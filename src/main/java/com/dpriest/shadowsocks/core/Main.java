0589887ca999f064defb29f62bfe4553cbf315a7
zhangwenhao <zhangwenhao@youzan.com>
2017-07-05 22:35:10 +0800

    add log

--- src/main/java/com/dpriest/shadowsocks/core/Main.java
+++ src/main/java/com/dpriest/shadowsocks/core/Main.java
@@ -1,9 +1,12 @@
 package com.dpriest.shadowsocks.core;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext(
@@ -11,5 +14,7 @@ public class Main {
         );
         HelloWorld obj = (HelloWorld) context.getBean("helloBean");
         obj.printHello();
+        logger.debug("main() is executed, value {}", "heeloBean");
+        logger.error("this is Error message", new Exception("Testing"));
     }
 }
